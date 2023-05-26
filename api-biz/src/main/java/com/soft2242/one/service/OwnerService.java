f661ed388c5c9dd369debbf8bf5d7c84dc3c87cf
lsc <1216634507@qq.com>
2023-05-26 11:38:13 +0800

    feat(custom): 家庭成员管理功能完善

--- api-biz/src/main/java/com/soft2242/one/service/OwnerService.java
+++ api-biz/src/main/java/com/soft2242/one/service/OwnerService.java
@@ -2,6 +2,7 @@ package com.soft2242.one.service;
 
 import com.soft2242.one.entity.OwnerEntity;
 import com.soft2242.one.mybatis.service.BaseService;
+import com.soft2242.one.vo.MyFamilyVo;
 import com.soft2242.one.vo.MyHouseListVo;
 
 import java.util.List;
@@ -13,4 +14,6 @@ import java.util.List;
  */
 public interface OwnerService extends BaseService<OwnerEntity> {
     List<MyHouseListVo> findMyHouseById(Long userId);
+    List<MyFamilyVo> findFamily(Long houseId);
+    boolean deleteFamily (Long ownerId,Long userId);
 }
--- api-biz/src/main/java/com/soft2242/one/service/impl/OwnerServiceImpl.java
+++ api-biz/src/main/java/com/soft2242/one/service/impl/OwnerServiceImpl.java
@@ -4,6 +4,7 @@ import com.soft2242.one.dao.OwnerDao;
 import com.soft2242.one.entity.OwnerEntity;
 import com.soft2242.one.mybatis.service.impl.BaseServiceImpl;
 import com.soft2242.one.service.OwnerService;
+import com.soft2242.one.vo.MyFamilyVo;
 import com.soft2242.one.vo.MyHouseListVo;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,15 @@ public class OwnerServiceImpl extends BaseServiceImpl<OwnerDao, OwnerEntity> imp
     @Override
     public List<MyHouseListVo> findMyHouseById(Long userId) {
         return baseMapper.findMyHouseById(userId);
+    }
+
+    @Override
+    public List<MyFamilyVo> findFamily(Long houseId) {
+        return baseMapper.findFamily(houseId);
+    }
+
+    @Override
+    public boolean deleteFamily(Long ownerId, Long userId) {
+        return baseMapper.deleteFamily(ownerId, userId)>0;
     }
 }
