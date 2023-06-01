7dffe800403cb72711715294e532f25792c03921
lsc <1216634507@qq.com>
2023-06-01 23:12:30 +0800

    feat(custom): 业主认证相关接口实现

--- api-biz/src/main/java/com/soft2242/one/controller/OwnerController.java
+++ api-biz/src/main/java/com/soft2242/one/controller/OwnerController.java
@@ -1,19 +1,22 @@
 package com.soft2242.one.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.soft2242.one.common.utils.Result;
 import com.soft2242.one.entity.OwnerEntity;
+import com.soft2242.one.entity.UserEntity;
 import com.soft2242.one.security.user.SecurityUser;
 import com.soft2242.one.service.OwnerService;
+import com.soft2242.one.service.UserService;
+import com.soft2242.one.vo.HouseOptionsVo;
 import com.soft2242.one.vo.MyFamilyVo;
 import com.soft2242.one.vo.MyHouseListVo;
+import com.soft2242.one.vo.OwnerInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,11 +31,12 @@ import java.util.List;
 @AllArgsConstructor
 public class OwnerController {
     private final OwnerService ownerService;
+    private final UserService userService;
 
     @PostMapping("findMyHouseList")
     @Operation(summary ="名下房屋列表")
     public Result<List<MyHouseListVo>> findMyHouseList(){
-        List<MyHouseListVo> list = ownerService.findMyHouseById(SecurityUser.getUser().getId());
+        List<MyHouseListVo> list = ownerService.findMyHouseById(SecurityUser.getUserId());
         return Result.ok(list);
     }
     @PostMapping("findFamily")
@@ -44,7 +48,7 @@ public class OwnerController {
     @DeleteMapping("deleteFamily")
     @Operation(summary = "删除家庭成员信息")
     private Result<String> deleteFamily(Long id){
-        if(ownerService.deleteFamily(id,SecurityUser.getUser().getId()))
+        if(ownerService.deleteFamily(id,SecurityUser.getUserId()))
             return Result.ok();
         else
             return Result.error(0,"删除失败");
@@ -53,7 +57,7 @@ public class OwnerController {
     @Operation(summary = "绑定家庭成员信息")
     private Result<String> addFamily(OwnerEntity owner){
         QueryWrapper<OwnerEntity> wrapper = new QueryWrapper<>();
-        wrapper.lambda().select(OwnerEntity::getId).eq(OwnerEntity::getHouseId,owner.getHouseId()).eq(OwnerEntity::getUserId,SecurityUser.getUser().getId())
+        wrapper.lambda().select(OwnerEntity::getId).eq(OwnerEntity::getHouseId,owner.getHouseId()).eq(OwnerEntity::getUserId,SecurityUser.getUserId())
                 .eq(OwnerEntity::getState,1).eq(OwnerEntity::getIdentity,0).eq(OwnerEntity::getDeleted,0);
         OwnerEntity one = ownerService.getOne(wrapper);
         if(one.getId()!=null){
@@ -65,5 +69,28 @@ public class OwnerController {
                 return Result.error(0,"绑定失败");
         }
         return Result.error(0,"绑定失败");
+    }
+    @PostMapping("houseOptions")
+    @Operation(summary = "获取房屋列表选项信息")
+    private Result<List<HouseOptionsVo>> houseOptions(){
+        List<HouseOptionsVo> list = ownerService.findHouseOptions();
+        return Result.ok(list);
+    }
+    @PostMapping("sbCertify")
+    @Operation(summary = "业主提交认证信息")
+    private Result<String> sbCertify(@RequestBody OwnerInfoVo ownerInfoVo){
+        OwnerEntity owner = OwnerEntity.builder().userId(SecurityUser.getUserId()).houseId(ownerInfoVo.getHouseId())
+                .realName(ownerInfoVo.getRealName()).phone(ownerInfoVo.getPhone()).identityCard(ownerInfoVo.getIdentityCard()).eContacts(ownerInfoVo.getEc())
+                .identity(0).gender(ownerInfoVo.getGender()).build();
+        ownerService.save(owner);
+        UpdateWrapper<UserEntity> wrapper = new UpdateWrapper<>();
+        wrapper.lambda().set(UserEntity::getGender,ownerInfoVo.getGender()).set(UserEntity::getRealName,ownerInfoVo.getRealName())
+                .set(UserEntity::getBirthday,ownerInfoVo.getBirthday()).set(UserEntity::getNation,ownerInfoVo.getNation())
+                .set(UserEntity::getMarriage,ownerInfoVo.getMarriage()).set(UserEntity::getAccountType,ownerInfoVo.getAccountType())
+                .set(UserEntity::getParty,ownerInfoVo.getParty()).set(UserEntity::getDomicileLocation,ownerInfoVo.getDomicileLocation())
+                .set(UserEntity::getRentalType,ownerInfoVo.getRentalType()).set(StringUtils.isNotEmpty(ownerInfoVo.getStayCard()),UserEntity::getStayCard,ownerInfoVo.getStayCard())
+                .set(UserEntity::getAddress,ownerInfoVo.getAddress()).eq(UserEntity::getId,SecurityUser.getUserId());
+        userService.update(new UserEntity(),wrapper);
+        return Result.ok();
     }
 }
--- api-biz/src/main/java/com/soft2242/one/dao/OwnerDao.java
+++ api-biz/src/main/java/com/soft2242/one/dao/OwnerDao.java
@@ -2,6 +2,7 @@ package com.soft2242.one.dao;
 
 import com.soft2242.one.entity.OwnerEntity;
 import com.soft2242.one.mybatis.dao.BaseDao;
+import com.soft2242.one.vo.HouseOptionsVo;
 import com.soft2242.one.vo.MyFamilyVo;
 import com.soft2242.one.vo.MyHouseListVo;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,4 +20,5 @@ public interface OwnerDao extends BaseDao<OwnerEntity> {
     List<MyHouseListVo> findMyHouseById(Long userId);
     List<MyFamilyVo> findFamily(Long houseId);
     int deleteFamily(@Param("id") Long id,@Param("userId") Long userId);
+    List<HouseOptionsVo> findHouseOptions();
 }
