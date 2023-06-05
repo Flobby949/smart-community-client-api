cbbaf54fa3b4eaa04c12250b0e0f9941d069ce19
aliyun2145454989 <2283582431@qq.com>
2023-06-05 15:43:11 +0800

    fix(custom): 文件上传

---
+++ api-plugin/file-plugin/src/main/java/com/soft2242/one/service/properties/LocalStorageProperties.java
@@ -0,0 +1,15 @@
+package com.soft2242.one.service.properties;
+
+import lombok.Data;
+
+@Data
+public class LocalStorageProperties {
+    /**
+     * 本地存储路径
+     */
+    private String path;
+    /**
+     * 资源起始路径
+     */
+    private String url = "upload";
+}
