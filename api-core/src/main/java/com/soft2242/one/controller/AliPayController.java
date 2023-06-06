bd7e0d3b69f3878ead485da0a4cd5fd73ccd9161
aliyun2145454989 <2283582431@qq.com>
2023-06-06 09:51:14 +0800

    fix(custom): 修复支付接口

--- api-core/src/main/java/com/soft2242/one/controller/AliPayController.java
+++ api-core/src/main/java/com/soft2242/one/controller/AliPayController.java
@@ -1,31 +1,120 @@
 package com.soft2242.one.controller;
 
 import com.alipay.api.AlipayApiException;
-import com.soft2242.one.pay.service.AliPayService;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.soft2242.one.dao.OrderMapper;
+import com.soft2242.one.entity.AliPay;
+import com.soft2242.one.entity.AlipayConfig;
+import com.soft2242.one.entity.Order;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 // xjlugv6874@sandbox.com
 // 9428521.24 - 30 = 9428491.24 + 30 = 9428521.24
 @RestController
 @RequestMapping("/alipay")
-@Tag(name = "支付宝支付", description = "支付宝支付")
-@Slf4j
 public class AliPayController {
-    @Resource
-    private AliPayService alipayService;
 
-    @GetMapping("/pay")
+    private static final String GATEWAY_URL = "https://openapi.alipaydev.com/gateway.do";
+    private static final String FORMAT = "JSON";
+    private static final String CHARSET = "UTF-8";
+    //签名方式
+    private static final String SIGN_TYPE = "RSA2";
+
+    @Resource
+    private AlipayConfig aliPayConfig;
+
+    @Resource
+    private OrderMapper orderMapper;
+
+
+    @GetMapping("/pay") // &subject=xxx&traceNo=xxx&totalAmount=xxx
     @Operation(summary = "支付宝支付")
-    public String pay(String traceNo, Double totalAmount, String subject, String alipayTraceNo, HttpServletResponse httpResponse) throws AlipayApiException, Exception {
-        String pay = alipayService.pay(traceNo, totalAmount, subject, alipayTraceNo, httpResponse);
-        return pay;
+    public void pay(AliPay aliPay, HttpServletResponse httpResponse) throws Exception {
+        AlipayClient alipayClient = new DefaultAlipayClient(GATEWAY_URL, aliPayConfig.getAppId(),
+                aliPayConfig.getAppPrivateKey(), FORMAT, CHARSET, aliPayConfig.getAlipayPublicKey(), SIGN_TYPE);
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+        request.setNotifyUrl(aliPayConfig.getNotifyUrl());
+        request.setBizContent("{\"out_trade_no\":\"" + aliPay.getTraceNo() + "\","
+                + "\"total_amount\":\"" + aliPay.getTotalAmount() + "\","
+                + "\"subject\":\"" + aliPay.getSubject() + "\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+        String form = "";
+        try {
+            form = alipayClient.pageExecute(request).getBody(); // 调用SDK生成表单
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println(form);
+        httpResponse.setContentType("text/html;charset=" + CHARSET);
+
+        httpResponse.getWriter().write(form);// 直接将完整的表单html输出到页面
+        httpResponse.getWriter().flush();
+        httpResponse.getWriter().close();
+
+    }
+
+    @PostMapping("/notify")  // 注意这里必须是POST接口
+    public String payNotify(HttpServletRequest request) throws Exception {
+        if (request.getParameter("trade_status").equals("TRADE_SUCCESS")) {
+            System.out.println("=========支付宝异步回调========");
+
+            Map<String, String> params = new HashMap<>();
+            Map<String, String[]> requestParams = request.getParameterMap();
+            for (String name : requestParams.keySet()) {
+                params.put(name, request.getParameter(name));
+                // System.out.println(name + " = " + request.getParameter(name));
+            }
+
+            String outTradeNo = params.get("out_trade_no");
+            String gmtPayment = params.get("gmt_payment");
+            String alipayTradeNo = params.get("trade_no");
+
+            String sign = params.get("sign");
+            String content = AlipaySignature.getSignCheckContentV1(params);
+            boolean checkSignature = AlipaySignature.rsa256CheckContent(content, sign, aliPayConfig.getAlipayPublicKey(), "UTF-8"); // 验证签名
+            // 支付宝验签
+            if (checkSignature) {
+                // 验签通过
+                System.out.println("交易名称: " + params.get("subject"));
+                System.out.println("交易状态: " + params.get("trade_status"));
+                System.out.println("支付宝交易凭证号: " + params.get("trade_no"));
+                System.out.println("商户订单号: " + params.get("out_trade_no"));
+                System.out.println("交易金额: " + params.get("total_amount"));
+                System.out.println("买家在支付宝唯一id: " + params.get("buyer_id"));
+                System.out.println("买家付款时间: " + params.get("gmt_payment"));
+                System.out.println("买家付款金额: " + params.get("buyer_pay_amount"));
+
+                // 查询订单
+                QueryWrapper<Order> queryWrapper = new QueryWrapper<>();
+                queryWrapper.eq("order_number", outTradeNo);
+                Order orders = orderMapper.selectOne(queryWrapper);
+
+                if (orders != null) {
+                    orders.setStatus(1);
+                    orders.setPayTime(LocalDateTime.now());
+                    orders.setOrderNumber(outTradeNo);
+                    orderMapper.updateById(orders);
+                }
+            }
+        }
+        return "success";
     }
 
 }
+
+
